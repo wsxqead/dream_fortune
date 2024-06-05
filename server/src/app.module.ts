db3f82cf5b77627b96ef793af7505cba24ff68f9
wsxqead <lsj122993@gmail.com>
2024-06-06 00:28:19 +0900

    서버 세팅 연동

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
+import { GuestModule } from './guest/guest.module'
+import { WalletModule } from './wallet/wallet.module'
+import { AuthModule } from './auth/auth.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [],
+  imports: [UsersModule, GuestModule, WalletModule, AuthModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService]
 })
