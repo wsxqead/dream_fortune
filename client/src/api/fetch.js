2566f7bf00f24b0eccf9dbdc7a9e9b7f8bee8014
wsxqead <lsj122993@gmail.com>
2024-06-06 15:10:57 +0900

    패치

---
+++ client/src/api/fetch.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+function getCookie(name) {
+  const cookieString = document.cookie;
+  const cookies = cookieString.split(";");
+  for (let cookie of cookies) {
+    const [cookieName, cookieValue] = cookie.trim().split("=");
+    if (cookieName === name) {
+      return decodeURIComponent(cookieValue);
+    }
+  }
+  return null;
+}
+
+const fetch = axios.create({
+  baseURL: process.env.API_URL || "http://localhost:4040",
+  withCredentials: true,
+});
+
+fetch.interceptors.request.use(
+  async (config) => {
+    const accessKey =
+      process.env.PUBLIC_ACCESS_COOKIE_KEY || "dream_access";
+    if (!accessKey) {
+      throw new Error("ACCESS_COOKIE_KEY is not defined");
+    }
+    const accessToken = getCookie(accessKey);
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  async (error) => {
+    return Promise.reject(error);
+  }
+);
+
+fetch.interceptors.response.use(
+  async (response) => {
+    return response;
+  },
+  async (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default fetch;
